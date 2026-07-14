09a2c264cbe08204d22018a56c9c105940a80af0
Joseman16 <josemaleoncarrera@gmail.com>
2026-07-14 11:54:56 -0500

    Creación de componentes de dar click y reiniciar, darle estilo y crear estados (hooks) y poder darle la logica al contador

--- contador-clicks/src/App.js
+++ contador-clicks/src/App.js
@@ -2,8 +2,27 @@
 import './App.css';
 /*Importamos nuestra imagen que esta en la carpeta imagenes*/
 import logo from './imagenes/logo.jpg';
+import Boton from './componentes/Boton.jsx'
+import Contador from './componentes/Contador.jsx';
+import { useState } from 'react';
+
+
 
 function App() {
+  //Esta constante sera un arreglo con 2 elementos importantes
+  //1. El valor del estado "numClics"
+  //2. la función que me va a permitir actualizarlo "setNumClics" (le asigne un nombre)
+  const[numClics, setNumClics ] = useState(0);
+
+  //Esto es uuna función flecha
+  const manejarClic = () =>{
+    setNumClics(numClics + 1); 
+  }
+
+  const reiniciarContador = () =>{
+    setNumClics(0); 
+  }
+  
   return (
     <div className="App">
       <div className='logo-contenedor'>
@@ -14,7 +33,17 @@ function App() {
         />
       </div>
       <div className='contenedor-principal'>
-        
+        <Contador numClics={numClics} />
+        <Boton 
+          texto='Clic'
+          esBotonDeClic={true} //Valor Booleano
+          manejarClic={manejarClic}
+        />
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
       </div>
     </div>
   );
--- contador-clicks/src/componentes/Boton.jsx
+++ contador-clicks/src/componentes/Boton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../hojas-de-estilos/Botones.css'
 
 function Boton ({texto, esBotonDeClic, manejarClic}) {
     return(
